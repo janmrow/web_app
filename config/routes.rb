8a9f6d1007e6e615d96a5830848986d8f575017f
Jasiek <jasiek92@gmail.com>
2015-03-28 18:51:56 +0100

    change routes, add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root             'pages#home'
   get 'blog'    => 'pages#blog'
   get 'about'   => 'pages#about'
   get 'contact' => 'pages#contact'
+
+  get 'signup'  => 'users#new'
+
 end
