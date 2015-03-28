5e39e966e74700fc4f02c9373b4d0683d4507fa4
Jasiek <jasiek92@gmail.com>
2015-03-28 17:46:23 +0100

    changes in pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,25 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact"
+  end
+
+  test "should get blog" do
+    get :blog
+    assert_response :success
+    assert_select "title", "Blog"
   end
 
 end
