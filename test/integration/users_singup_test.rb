ac1ce74102ac5627d8bc42a45affdcd5cf770594
Jasiek <jasiek92@gmail.com>
2015-03-29 23:43:29 +0200

    test for signup page

---
+++ test/integration/users_singup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSingupTest < ActionDispatch::IntegrationTest
+
+  test "valid signup data" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, user: { name: "Tim", email: "tim@post.com",
+                              password: "pass123", password_confirmation: "pass123"}
+    end
+  end
+
+  test "invalind signup data" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "Tim", email: "tom@",
+              password: "abc", password_confirmation: ""}
+    end
+  end
+end
