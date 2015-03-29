30e78b9293a7643487aeecbf64ca5a3aef0ec411
Jasiek <jasiek92@gmail.com>
2015-03-29 21:58:22 +0200

    user model and tests for user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+            uniqueness: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup # automatically run before each test
+    @user = User.new(name: "John", email: "john@post.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should exist" do 
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should exist" do 
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do 
+    another_user = User.new
+    another_user.email = @user.email
+    assert_not another_user.valid?
+  end
+
+  test "name should not be longer than 50 chars" do 
+    @user.name = "x" * 51
+    assert_not @user.valid?
+  end
+
+  test "email shoul not be longer than 255 chars" do 
+    @user.email = "x" * 250 + "@post.com"
+    assert_not @user.valid?
+  end
+
+  test "system should accept valid email" do 
+    @user.email = "tom@post.com"
+    assert @user.valid?
+  end
+
+  test "system should not accept invalid email" do 
+    @user.email = "tom@post"
+    assert_not @user.valid?
+  end
+end
